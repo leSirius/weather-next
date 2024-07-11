8f7e8bfe0901f113ccaac933bc2c742fa90f28e6
leSirius <Calvin.J.Tsui@gmail.com>
2024-07-12 06:43:50 +0800

    643

--- app/ui/search/weather-info/tail-board-server.js
+++ app/ui/search/weather-info/tail-board-server.js
@@ -19,13 +19,13 @@ export default async function TailBoardServer({id}) {
   return (
     <div className='grid grid-cols-3 gap-4 h-full text-card'>
       <Cube>{(!!nowData.windScale)&&<WindServer nowData={nowData}></WindServer>}</Cube>
-      <Cube>{moonData.moonPhase
+      <Cube>{(!!moonData.moonPhase)
         ? <MoonCarouselServer data={moonData} today={today}></MoonCarouselServer>
         : moonData
       }</Cube>
-      <Cube>{sunData.sunrise
+      <Cube>{(!!sunData.sunrise)
         ? <SunInfoServer data={sunData}></SunInfoServer>
-        : sunData.toString()+Reflect.ownKeys(sunData)
+        : Reflect.ownKeys(sunData).join(', ')
       }</Cube>
     </div>
   )
